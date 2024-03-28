9451b2a54493041e5631d8798a1185f76fa369f8
DavyAndrade <hallszindementahhh@gmail.com>
2024-03-28 00:01:42 -0300

    v1

--- commands.py
+++ commands.py
@@ -11,13 +11,13 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 def horario_brasilia():
     fuso_horario_brasilia = pytz.timezone('America/Sao_Paulo')
     horario_atual_brasilia = datetime.now(fuso_horario_brasilia)
-    horario_formatado = horario_atual_brasilia.strftime("%d-%m-%Y %H:%M:%S")
+    horario_formatado = horario_atual_brasilia.strftime("%d/%m/%Y às %H:%M:%S")
     return horario_formatado
 
 @bot.command(name='prodígio', help='Roll de Prodígio.', aliases=["Prodígio", "Prod", "prod"])
 async def prod(ctx):
     prod = random.randint(1, 10000)
-
+    
     imgPlayer = ["https://64.media.tumblr.com/025aeae9e639fd300e3353993e1e3e84/b2c25907ead440a2-d4/s1280x1920/1ac0f2ea74df3faa6d5c4ecaa855d95d9a5c001f.gif", "https://i.pinimg.com/564x/b9/5c/d3/b95cd3429d971c1a9915485b347296de.jpg"]
     imgProd = ["https://64.media.tumblr.com/fe90fb11e31c84f26e1650d76404efe2/732812b37c31da07-34/s540x810/2ce9a0f1b17e89fdf1bcf9a0a58cd95d6a9577af.gif", "https://media.tenor.com/UfzsR3iHAOsAAAAM/ao-ashi-haruhisa-kuribayashi.gif"]
 
@@ -28,8 +28,8 @@ async def prod(ctx):
             color=discord.Colour.red()
         )
         embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar),
-        embed.set_image(url=(f"{imgProd}"))
-        embed.set_footer(text=(f"{horario_brasilia()}"))
+        embed.set_image(url=(f"{random.choice(imgProd)}"))
+        embed.set_footer(text=(f"Comando realizado em {horario_brasilia()}"))
         await ctx.send(embed=embed)
 
     else:
@@ -38,8 +38,8 @@ async def prod(ctx):
             description="Você é apenas um jogador comum. Se quiser ter sucesso, melhor começar a treinar arduamente..."
         )
         embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar)
-        embed.set_image(url=(f"{imgPlayer}"))
-        embed.set_footer(text=(f"{horario_brasilia()}"))
+        embed.set_image(url=(f"{random.choice(imgPlayer)}"))
+        embed.set_footer(text=(f"Comando realizado em {horario_brasilia()}"))
         await ctx.send(embed=embed)
 
 
--- main.py
+++ main.py
@@ -17,7 +17,7 @@ setup(bot)
 def horario_brasilia():
     fuso_horario_brasilia = pytz.timezone('America/Sao_Paulo')
     horario_atual_brasilia = datetime.now(fuso_horario_brasilia)
-    horario_formatado = horario_atual_brasilia.strftime("%d-%m-%Y %H:%M:%S")
+    horario_formatado = horario_atual_brasilia.strftime("%d/%m/%Y às %H:%M:%S")
     return horario_formatado
 
 @bot.command(name='teste', help='test', aliases=["Teste", "Test", "test"])
@@ -30,7 +30,7 @@ async def test(ctx):
     embed.set_author(name=(f"{bot.user.name} Maravilhita"), icon_url=bot.user.avatar)
     embed.set_thumbnail(url="https://i.pinimg.com/564x/71/8c/82/718c82058e01ddfd846562fb973f5060.jpg")
     embed.set_image(url="https://i.pinimg.com/originals/f8/d9/17/f8d917dd4dd4f21fb3f6f72d814afca4.gif")
-    embed.set_footer(text=(f"{horario_brasilia()}"), icon_url=bot.user.avatar)
+    embed.set_footer(text=(f"Comando realizado em {horario_brasilia()}"), icon_url=bot.user.avatar)
     await ctx.send(embed=embed)
 
 bot.run('')
